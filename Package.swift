8877e2cfe87282d4ad17c0b4a42cb2cd246f1053
Ezequiel Santos <ezequiel.santos@miniclip.com>
2026-01-05 17:19:23 +0000

    Swift Package update

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
             targets: ["AccessibleFonts"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
